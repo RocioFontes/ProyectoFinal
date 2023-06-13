37deccb71c36dc28c174e5ae05846e18b86e3bf9
Rocio Fontes <fontesrocio2002@gmail.com>
2023-06-12 22:07:37 -0300

    probando

--- logica/funcionescarrusel.js
+++ logica/funcionescarrusel.js
@@ -1,30 +1,29 @@
-const carrusel = document.querySelector (".carrusel-items");
+const carrusel = document.querySelector(".carrusel-items");
 
 let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
 let intervalo = null;
-let step = 1;
-
+let step = 2;
 const start = () => {
-    intervalo = setInterval (function () {
+    intervalo = setInterval(function () {
         carrusel.scrollLeft = carrusel.scrollLeft + step;
-        if(carrusel.scrollLeft === maxScrollLeft) {
+        if (carrusel.scrollLeft === maxScrollLeft) {
             step = step * -1;
-        } else if (carrusel.scrollLeft === 0){
+        } else if (carrusel.scrollLeft === 0) {
             step = step * -1;
         }
-    },10);
+    }, 50);
 };
 
 const stop = () => {
-    clearInterval (intervalo);
+    clearInterval(intervalo);
 };
 
-carrusel.addEventListener ("mouseover", () =>{
-    stop ();
+carrusel.addEventListener("mouseover", () => {
+    stop();
 })
 
-carrusel.addEventListener ("mouseout", () =>{
-    start ();
+carrusel.addEventListener("mouseout", () => {
+    start();
 })
 
 start(); 
